a6b9a36f358de09b7cd0b308a74fa9c1c618df67
gitjcc <jc_jesse@hotmail.com>
2017-02-23 14:24:32 +0800

    format

--- src/components/tree-vue/js/x-tree-vue.js
+++ src/components/tree-vue/js/x-tree-vue.js
@@ -39,7 +39,9 @@ function _getTreeRoot(arrayIn) {
     // function unique(array) {
     //     var n = [];
     //     for (var i = 0; i < array.length; i++) {
-    //         if (n.indexOf(array[i]) == -1) n.push(array[i]);
+    //         if (n.indexOf(array[i]) == -1) {
+    //             n.push(array[i]);
+    //         }
     //     }
     //     return n;
     // }
