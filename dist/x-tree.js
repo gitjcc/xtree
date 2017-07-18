0b6226415a9a95625f620b9a33e70d473f8035cd
gitjcc <jc_jesse@hotmail.com>
2017-07-18 17:35:35 +0800

    去掉多余代码

--- dist/x-tree.js
+++ dist/x-tree.js
@@ -4,17 +4,6 @@
     return new tree(options);
   };
 
-  // if (!Array.prototype.indexOf) {
-  //   Array.prototype.indexOf = function (obj, start) {
-  //     for (var i = (start || 0), j = this.length; i < j; i++) {
-  //       if (this[i] === obj) {
-  //         return i;
-  //       }
-  //     }
-  //     return -1;
-  //   }
-  // }
-
   var defOpt = {
     dom: '', //jqueryDom
     position: 'absolute',
